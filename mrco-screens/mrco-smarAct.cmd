9ab115b2da24f347c1c14a49d36cecea1ef0db0f
Zachary Lentz <zlentz@slac.stanford.edu>
2026-02-25 17:00:49 -0800

    Automatic backup @ Wed Feb 25 17:00:49 PST 2026

--- mrco-screens/mrco-smarAct.cmd
+++ mrco-screens/mrco-smarAct.cmd
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 typhos --scrollable true --size 1450,850 --display-type "embedded" --layout "vertical" \
-            "pcdsdevices.epics_motor.SmarAct[{'prefix':'TMO:MCS2:02:m1', 'name':'mrco_smarAct_m1'}]" \
-            "pcdsdevices.epics_motor.SmarAct[{'prefix':'TMO:MCS2:02:m2', 'name':'mrco_smarAct_m2'}]" \
-            "pcdsdevices.epics_motor.SmarAct[{'prefix':'TMO:MCS2:02:m3', 'name':'mrco_smarAct_m3'}]" \
-            "pcdsdevices.epics_motor.SmarAct[{'prefix':'TMO:MCS2:02:m4', 'name':'mrco_smarAct_m4'}]" \
-            "pcdsdevices.epics_motor.SmarAct[{'prefix':'TMO:MCS2:02:m5', 'name':'mrco_smarAct_m5'}]" \
-            "pcdsdevices.epics_motor.SmarAct[{'prefix':'TMO:MCS2:02:m6', 'name':'mrco_smarAct_m6'}]"  &
+            "pcdsdevices.epics_motor.SmarAct[{'prefix':'TMO:MCS2:02:m2', 'name':'mrco_smarAct_x'}]" \
+            "pcdsdevices.epics_motor.SmarAct[{'prefix':'TMO:MCS2:02:m1', 'name':'mrco_smarAct_y'}]" \
+            "pcdsdevices.epics_motor.SmarAct[{'prefix':'TMO:MCS2:02:m3', 'name':'mrco_smarAct_theta'}]" &
 
