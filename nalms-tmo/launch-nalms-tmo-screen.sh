588d50399f708e508eac90dc1f842370ca81616c
Zachary Lentz <zlentz@slac.stanford.edu>
2026-04-15 17:01:04 -0700

    Automatic backup @ Wed Apr 15 17:01:04 PDT 2026

--- nalms-tmo/launch-nalms-tmo-screen.sh
+++ nalms-tmo/launch-nalms-tmo-screen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source /cds/group/pcds/pyps/conda/dev_conda
+source /cds/group/pcds/pyps/conda/kthlciekfjvlbn
 
-slam --topics TMO-alarms --bootstrap-servers 172.24.5.232:9094
+slam --topics GMDXGMD,TMO-alarms --bootstrap-servers 172.24.5.232:9094
